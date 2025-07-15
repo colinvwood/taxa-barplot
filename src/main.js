54b828cb23d8e4503694c1caf900468c84c3fd47
Colin Wood <woodcolin38@gmail.com>
2025-07-15 14:16:00 -0700

    move to svelte 5

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { app } from "svelte";
-import App from "./App.svelte";
-
-app(App, { target: document.body });
-
-export default app;
--- tests/taxonomy.test.js
+++
@@ -1,9 +0,0 @@
-import { test, expect, vi } from 'vitest';
-
-import { renderCurrentView } from '../src/util/taxonomy.js';
-
-
-test('renderCurrentView', async () => {
-
-    expect(1).toEqual(1);
-})
