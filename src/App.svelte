0e486d588d989afcf0c6135be1fc963af13e0fed
Colin Wood <woodcolin38@gmail.com>
2025-08-12 15:28:54 -0700

    promise stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
+    import Plot from "./lib/Plot.svelte";
+    import FeatureSort from "./lib/FeatureSort.svelte";
+    import FeatureFilters from "./lib/FeatureFilters.svelte";
+    import SampleControls from "./lib/sample-controls/SampleControls.svelte";
+    import { sampleManager } from "./classes/samples";
+
+    const smPromise = sampleManager.parseFeatureTable("table.csv");
+
+    const parsedPromise = smPromise.then((r) => {
+        sampleManager.setPlotDimensions(500, 500);
+
+        const tPromise = sampleManager.taxonomy.parse("taxonomy.tsv");
+        const mdPromise = sampleManager.metadata.parse("metadata.tsv");
+        const cPromise = sampleManager.colors.parse("colors.csv");
+
+        return Promise.all([tPromise, mdPromise, cPromise]);
+    });
 </script>
 
-<h1>Hello Bubba!</h1>
-
-<style>
-</style>
+{#await parsedPromise}
+    <p>Loading...</p>
+{:then}
+    <Plot />
+    <FeatureSort />
+    <FeatureFilters />
+    <SampleControls />
+{:catch error}
+    <p>An error occured: {error}</p>
+{/await}
